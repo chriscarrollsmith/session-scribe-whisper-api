d237d7076548df597deff0e5ca16521b153ade52
Christopher Carroll Smith <75859865+chriscarrollsmith@users.noreply.github.com>
2024-02-17 12:09:23 -0500

    Update test_api.py

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,11 @@
 import requests
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def test_transcribe_endpoint() -> None:
     # Define the URL
-    url = 'https://chriscarrollsmith--whisper-audio-video-transcriber-api-v-4c6a21.modal.run/api/transcribe'
+    url = os.environ["MODAL_DOMAIN] + '/api/transcribe'
 
     # Define the headers
     headers = {
@@ -12,7 +14,7 @@ def test_transcribe_endpoint() -> None:
 
     # Define the payload
     data = {
-        "src_url": "https://storage.googleapis.com/session-scribe-bucket/1689897654847-audio-1689897655558.mp3",
+        "src_url": "https://storage.googleapis.com/" + os.environ["BUCKET_NAME] + "/1689897654847-audio-1689897655558.mp3",
         "unique_id": 987654,
         "session_title": "Session Title Here",
         "presenters": "Presenters Here",
